ef94474d6c60848bb37b8a63cb63907a2a39a992
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-03-19 20:38:02 +0530

    mail update

--- util/sendMail.js
+++ util/sendMail.js
@@ -4,11 +4,15 @@ const sendMail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: false,
         service: process.env.SMTP_SERVICE,
         auth:{
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
         },
+        tls: {
+            rejectUnauthorized: false // Disables SSL certificate verification
+          }
     });
 
     const mailOption = {
